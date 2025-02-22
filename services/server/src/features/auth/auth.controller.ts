e1da8b53ec309bead03682693272518ac98ccaa6
Assaf Koren <assafnoahkoren@gmail.com>
2025-02-23 00:10:53 +0200

    .

--- services/server/src/features/auth/auth.controller.ts
+++ services/server/src/features/auth/auth.controller.ts
@@ -2,22 +2,34 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { IsNotEmpty } from 'class-validator';
 import { IsString } from 'class-validator';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 
 
-export class SignInDto {
+class SignInDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The phone number of the user',
+    example: '+972541234567',
+  })
   phoneNumber: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The code of the user',
+    example: '1234',
+  })
   code: string;
 }
 
 export class VerifyPhoneNumberDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({
+    description: 'The phone number of the user',
+    example: '+972541234567',
+  })
   phoneNumber: string;
 }
 
@@ -26,35 +38,11 @@ export class VerifyPhoneNumberDto {
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  
-  @ApiBody({ 
-    type: SignInDto,
-    description: 'Sign in to the application',
-    examples: {
-      'Sign in': {
-      value: {
-          phoneNumber: '+972541234567',
-          code: '1256',
-        },
-      }
-    }
-  })
   @Post('sign-in')
   signIn(@Body() body: SignInDto) {
     return this.authService.signIn(body.phoneNumber, body.code);
   }
 
-  @ApiBody({ 
-    type: VerifyPhoneNumberDto,
-    description: 'Verify phone number',
-    examples: {
-      'Verify phone number': {
-        value: {
-          phoneNumber: '+972541234567',
-        },
-      }
-    }
-  })
   @Post('verify-phone-number')
   verifyPhoneNumber(@Body() body: VerifyPhoneNumberDto) {
     return this.authService.verifyPhoneNumber(body.phoneNumber);
