173057251133e35d33dcf319b5b06063c2de6d50
Assaf Koren <assafnoahkoren@gmail.com>
2025-03-05 00:21:41 +0200

    .

--- services/mobile-app/src/features/login/LoginPage.tsx
+++ services/mobile-app/src/features/login/LoginPage.tsx
@@ -2,13 +2,14 @@ import { observer } from 'mobx-react-lite';
 import { authContext } from '../../global-stores/auth-store';
 import { Button, Input, Space, Stack, Text, Title } from '@mantine/core';
 import { IconPhone } from '@tabler/icons-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 const LoginPage = () => {
   const authStore = authContext.use();
+  const [phoneNumber, setPhoneNumber] = useState('');
 
   const handleEnterPress = (e: KeyboardEvent) => {
     if (e.key === 'Enter') {
-      alert('Enter pressed');
+      authStore.m_verifyPhoneNumber.mutate({phoneNumber: phoneNumber});
     }
   };
 
@@ -23,7 +24,10 @@ const LoginPage = () => {
         <Title className='c-dark' order={1}>על הדרך</Title>
         <Space h={8} />
         <Stack gap={4} className="bg-white p-4 rounded-2xl w-full" align='center'>
-          <Input className='w-full' variant='filled' inputMode='tel' size='md' placeholder="052-123-4567" leftSection={<IconPhone className='opacity-50' />} />
+          <Input className='w-full' variant='filled' inputMode='tel' size='md' placeholder="052-123-4567" leftSection={<IconPhone className='opacity-50' />}
+            value={phoneNumber}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
           <Text className='opacity-30'>אין צורך בהרשמה, הזן טלפון והתחבר</Text>
           <Space h={16} />
           <Button variant='filled' color='blue' size='md'>התחבר</Button>
